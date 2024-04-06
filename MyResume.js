4aec8c3e54d8b4cb8bceda3e8281f9a0309c984e
Alex <acordova1@live.maryville.edu>
2024-04-06 00:19:13 -0500

    Added functionality to back/next buttons as well as add/delete school in education section of form

--- MyResume.js
+++ MyResume.js
@@ -1,6 +1,6 @@
 //Creating Dynamic Fields
-const addSchoolBtn = document.querySelector("#add-school");
-const education = document.getElementById("education");
+const addSchoolBtn = document.querySelector("form .add-school");
+const education = document.querySelector("form .education");
 
 function addSchool(){
     //Add School Label and Input Elements
@@ -26,9 +26,14 @@ function addSchool(){
     var addGraduationLabel = document.createElement("label");
 
     addGraduationLabel.setAttribute("for", "degree");
-    addGraduationLabel.innerHTML = "Geaduation Year ";
+    addGraduationLabel.innerHTML = "Graduation Year ";
     addGraduation.type="text";
     addGraduation.setAttribute("placeholder", "Graduation Year");
+
+    //Add Delete Button
+    var deleteBtn = document.createElement("button")
+    deleteBtn.innerHTML = "Delete School";
+    deleteBtn.addEventListener("click", deleteSchool);
 
     //Adding Div
     const educationDiv = document.createElement("div");
@@ -38,7 +43,46 @@ function addSchool(){
     educationDiv.appendChild(addDegree);
     educationDiv.appendChild(addGraduationLabel);
     educationDiv.appendChild(addGraduation);
+    educationDiv.appendChild(deleteBtn)
     education.appendChild(educationDiv);
+    educationDiv.before(addSchoolBtn)
 }
 
+function deleteSchool() {
+    this.parentElement.remove();
+}
+
+
 addSchoolBtn.addEventListener("click", addSchool);
+
+//Dynamic Multi-Step Form Functionality
+const steps =Array.from(document.querySelectorAll("form .step"));
+const nextBtn = document.querySelectorAll("form .next");
+const backBtn = document.querySelectorAll("form .back");
+const form =document.querySelector("form");
+
+nextBtn.forEach(button=>{
+    button.addEventListener("click", (e) => {
+        changeStep("next");
+    })
+})
+
+backBtn.forEach(button=>{
+    button.addEventListener("click", (e) => {
+        changeStep("back");
+    })
+})
+
+function changeStep(btn) {
+    let index = 0;
+    const active = document.querySelector("form .step.active");
+    index = steps.indexOf(active)
+    steps[index].classList.remove("active");
+    if (btn === "next"){
+        index ++;
+    }else if(btn === "back"){
+        index--;
+    }
+
+    steps[index].classList.add("active");
+}
